c7919d9a7f7797da2634f92c8d2c21f03817823c
raphaelthuillier <raphael.thuillier@isoskele.fr>
2025-10-07 09:15:54 +0200

    Create WalletScreen.tsx

---
+++ app/src/screens/WalletScreen.tsx
@@ -0,0 +1,2 @@
+import { View, Text } from "react-native";
+export default function WalletScreen(){ return <View style={{ padding:16 }}><Text>Cagnotte • En attente 0,00 € • Validés 0,00 €</Text></View>; }
